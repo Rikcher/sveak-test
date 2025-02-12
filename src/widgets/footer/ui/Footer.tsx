17406dfea7013aad85ee7718f3c42a9a85710f24
Rikcher <nick.richardson.4884@gmail.com>
2025-02-13 05:09:49 +0700

    feat: Create footer

--- src/widgets/footer/ui/Footer.tsx
+++ src/widgets/footer/ui/Footer.tsx
@@ -1,6 +1,14 @@
-// import React from 'react';
+import './Footer.scss';
+import { useTranslation } from 'react-i18next';
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  const { t } = useTranslation();
+
+  return (
+    <footer className="footer">
+      <p className="footer__title">{t('Footer.title')}</p>
+      <p className="footer__text">{t('Footer.text')}</p>
+    </footer>
+  );
 };
 export default Footer;
