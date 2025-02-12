b8a6014ca287c34797874dcbce5ed48575fc889a
Rikcher <nick.richardson.4884@gmail.com>
2025-02-12 14:14:04 +0700

    feat: setup translations

--- src/app/providers/Providers.tsx
+++ src/app/providers/Providers.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
 import { ThemeProvider } from '@/entities/theme';
+import { I18nextProvider } from 'react-i18next';
+import i18n from '../../../i18n';
 
 interface Providers {
   readonly children: React.JSX.Element;
 }
 
 const Providers: React.FC<Providers> = ({ children }) => {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  return (
+    <I18nextProvider i18n={i18n}>
+      <ThemeProvider>{children}</ThemeProvider>
+    </I18nextProvider>
+  );
 };
 
 export default Providers;
--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,12 +1,20 @@
 import { useTheme } from '@/entities/theme';
+import { useTranslation } from 'react-i18next';
 
 const HomePage = () => {
   const { toggleTheme } = useTheme();
-
+  const { t, i18n } = useTranslation();
+  const handleLanguageChange = (language: string) => {
+    i18n.changeLanguage(language);
+  };
   return (
     <div>
-      Home page
+      {t('HomePage.hello')}
       <button onClick={toggleTheme}>theme</button>
+      <div>
+        <button onClick={() => handleLanguageChange('en')}>English</button>
+        <button onClick={() => handleLanguageChange('ru')}>Русский</button>
+      </div>
     </div>
   );
 };
